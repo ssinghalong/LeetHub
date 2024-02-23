3d1c6ff820293cf0234b8756e4a62348adc30537
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-02-23 18:46:38 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (77.18%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -47,13 +47,15 @@ public:
 
     //tabulation peeche se aage
     int rob(vector<int>& nums) {
-        int n =nums.size();
-        vector<int> dp(n+1,-1);
-        dp[n-1] = nums[n-1];
-        dp[n]=0;
+        int n = nums.size();
+        int next = 0;
+        int curr;
+        int prev = nums[n-1];
         for(int i=n-2;i>=0;i--){
-            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+            curr = max(nums[i]+next,prev);
+            next = prev;
+            prev = curr;
         }
-        return dp[0];
+        return prev;
     }
 };
