a0ed86e6e561408910cb6c0b29d67f3ebe6741fb
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-02-23 17:52:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (7.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int call(vector<int> &nums,vector<int> &dp,int &n,int ind){
-        if(ind<0) return 0;
+    int solve2(vector<int> &nums,int ind){
+        //base
+        if(ind>=nums.size()){
+            return 0;
+        }
+
+        //iter
+        int include = nums[ind]+solve2(nums,ind+2);
+        int exclude = solve2(nums,ind+1);
+        int ans = max(include,exclude);
+        return ans;
+    }
+
+    int solve(vector<int> &nums,int ind,vector<int> &dp){
+        if(ind>=nums.size()) return 0;
         if(dp[ind]!=-1) return dp[ind];
-        int t = call(nums,dp,n,ind-1);
-        int u = nums[ind]+call(nums,dp,n,ind-2);
-        dp[ind] = max(t,u);
+
+        dp[ind] = max(nums[ind]+solve(nums,ind+2,dp),solve(nums,ind+1,dp));
         return dp[ind];
     }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
         vector<int> dp(n+1,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        int ans = call(nums,dp,n,n-1);
+        int ans = solve(nums,0,dp);
         return ans;
     }
 };
