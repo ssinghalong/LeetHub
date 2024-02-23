a1e215f0163623f0b678c4492baa10ffd3fe4175
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-02-23 18:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (42.15%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //normal recursion
     // int solve2(vector<int> &nums,int ind){
     //     //base
     //     if(ind>=nums.size()){
@@ -13,6 +14,7 @@ public:
     //     return ans;
     // }
 
+    //memoization
     // int solve(vector<int> &nums,int ind,vector<int> &dp){
     //     if(ind>=nums.size()) return 0;
     //     if(dp[ind]!=-1) return dp[ind];
@@ -28,16 +30,30 @@ public:
     //     return ans;
     // }
 
+    //tablulation aage se peeche
+    // int rob(vector<int>& nums) {
+    //     int n = nums.size();
+    //     if(n==0) return 0;
+    //     if(n==1) return nums[0];
+    //     vector<int> dp(n+1,-1);
+    //     dp[0]=nums[0];
+    //     dp[1]=max(nums[0],nums[1]);
+    //     for(int i=2;i<n;i++){
+    //         dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
+    //     }
+    //     return dp[n-1];
+    // }
+
+
+    //tabulation peeche se aage
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
+        int n =nums.size();
         vector<int> dp(n+1,-1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
+        dp[n-1] = nums[n-1];
+        dp[n]=0;
+        for(int i=n-2;i>=0;i--){
+            dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
         }
-        return dp[n-1];
+        return dp[0];
     }
 };
