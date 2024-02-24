672640bdbfe070ecd4b374f6690dda97fc38dd55
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-02-24 11:00:51 +0530

    Time: 20 ms (90.56%) | Memory: 15.9 MB (66.13%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    int solve(vector<int> &coins,int amount,vector<int> &dp){
-        //base
-        if(amount==0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        //iter
-        int mini = INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int toto = 0;
-            if(amount-coins[i]>=0){
-                toto = solve(coins,amount-coins[i],dp);
-                if(toto==INT_MAX) continue;
-                int temp = 1 + toto;
-                mini = min(temp,mini);
+    // int solve(vector<int> &coins,int amount,vector<int> &dp){
+    //     //base
+    //     if(amount==0) return 0;
+    //     if(dp[amount]!=-1) return dp[amount];
+    //     //iter
+    //     int mini = INT_MAX;
+    //     for(int i=0;i<coins.size();i++){
+    //         int toto = 0;
+    //         if(amount-coins[i]>=0){
+    //             toto = solve(coins,amount-coins[i],dp);
+    //             if(toto==INT_MAX) continue;
+    //             int temp = 1 + toto;
+    //             mini = min(temp,mini);
+    //         }
+    //     }
+    //     dp[amount]=mini;
+    //     return dp[amount];
+    // }
+
+    int solve(vector<int> &coins,int amount){
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int value=1;value<=amount;value++){
+            int mini = INT_MAX;
+            for(int i=0;i<coins.size();i++){
+                if(value-coins[i]>=0){
+                    int t = dp[value-coins[i]];
+                    if(t!=INT_MAX){
+                        int ans = 1+t;
+                        mini = min(mini,ans);
+                    }
+                }
             }
+            dp[value]=mini;
         }
-        dp[amount]=mini;
         return dp[amount];
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        int ans = solve(coins,amount,dp);
+        // vector<int> dp(amount+1,-1);
+        int ans = solve(coins,amount);
         if(ans==INT_MAX) return -1;
         return ans;
     }
