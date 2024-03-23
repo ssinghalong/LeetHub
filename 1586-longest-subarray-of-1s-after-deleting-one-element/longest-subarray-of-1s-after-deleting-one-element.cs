8788c56d8ea343384b2bd012adc7335fd75a7ca5
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-03-23 14:35:32 +0530

    Time: 210 ms (22.63%) | Memory: 54.4 MB (28.63%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int LongestSubarray(int[] nums) {
+        int l=0,r=0,k=1;
+        int n = nums.Length;
+        while(r<n){
+            if(nums[r]==0){
+                k--;
+            }
+            if(k<0){
+                if(nums[l]==0){
+                    k++;
+                }
+                l++;
+            }
+            r++;
+        }
+        return r-l-1;
+    }
+}
