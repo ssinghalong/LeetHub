1f7bd603a283b9c4d74ee9b66309d935d0a2a460
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-03-23 14:56:36 +0530

    Time: 64 ms (34.62%) | Memory: 40.4 MB (51.94%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int LargestAltitude(int[] gain) {
+        int ma=0,su=0;
+        for(int i=0;i<gain.Length;i++){
+            su+=gain[i];
+            ma=Math.Max(ma,su);
+        }
+        return ma;
+    }
+}
