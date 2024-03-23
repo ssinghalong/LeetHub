8534c4aeec6661432720d6ae766bbddb8394ecbf
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-03-23 13:57:03 +0530

    Time: 213 ms (13.16%) | Memory: 61.5 MB (76.74%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int LongestOnes(int[] nums, int k) {
+        int ma=0;
+        int l=0;
+        int r=0;
+        int count=0;
+        int n = nums.Length;
+        while(r<n){
+            while(r<n){
+                if(nums[r]==0){
+                    if(count==k) break;
+                    count++;
+                }
+                r++;
+            }
+            Console.WriteLine();
+            ma=Math.Max(ma,r-l);
+            while(l<n && nums[l]==1){
+                l++;
+            }
+            if(l<n && nums[l]==0){
+                count--;
+                l++;
+            }
+        }
+        return ma;
+    }
+}
