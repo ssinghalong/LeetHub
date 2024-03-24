64ca1e216b5da3f5244c472b31b12fdb618a1b92
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-03-24 15:07:05 +0530

    Time: 260 ms (83.52%) | Memory: 53.1 MB (100.00%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cs
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
         var hs = new HashSet<int> ();
+        GC.Collect();
         for(int i=0;i<nums.Length;i++){
             if(hs.Contains(nums[i])){
                 return nums[i];
