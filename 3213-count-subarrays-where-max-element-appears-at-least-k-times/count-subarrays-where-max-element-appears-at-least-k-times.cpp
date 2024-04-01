96c7e362961446a8d3c33353a9738d9eed936e1d
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-04-01 06:34:23 +0530

    Time: 138 ms (28.92%) | Memory: 123.6 MB (17.63%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,27 +1,26 @@
-#define ll long long
-
 class Solution {
 public:
-    long long countSubarrays(vector<int>& nums, int k) {
-        ll ma=0,n=nums.size();
-        for(int i=0;i<n;i++){
-            if(ma<nums[i]){
-                ma=nums[i];
+    long long countSubarrays(std::vector<int>& nums, int k) {
+        const int N = nums.size();
+        const int max_elem = *std::max_element(std::begin(nums), std::end(nums));
+
+        std::vector<int> positions;
+        for (int i = 0; i < N; ++i) {
+            if (nums[i] == max_elem) {
+                positions.push_back(i);
             }
         }
-        ll l=0,res=0,maco=0;
-        for(ll i=0;i<n;i++){
-            if(nums[i]==ma){
-                maco++;
-            }
-            while(maco>k || (l<i && maco==k && nums[l]!=ma)){
-                if(nums[l]==ma){
-                    maco--;
-                }
-                l++;
-            }
-            if(maco==k) res+=l+1;
+        const int M = positions.size();
+        
+        int prev_idx = -1;
+        long long counter = 0;
+        for (int r = k - 1; r < M; ++r) {
+            const int l = r - k + 1;
+            const long long L = positions[l] + 1 - (prev_idx + 1); 
+            const long long R = N - positions[r];
+            counter += L * R;
+            prev_idx = positions[l];
         }
-        return res;
+        return counter;
     }
 };
