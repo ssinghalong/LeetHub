6f433cb2b1f32bc7e4ac5928f20cd629dcddaa3b
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-04-05 20:06:23 +0530

    Time: 56 ms (69.62%) | Memory: 39.9 MB (79.11%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string MakeGood(string s) {
+        var st = new Stack<char> ();
+        for(int i=0;i<s.Length;i++){
+            if(st.Count>0 && Math.Abs(st.Peek()-s[i])==32){
+                st.Pop();
+            }
+            else{
+                st.Push(s[i]);
+            }
+        }
+        string ans = "";
+        while(st.Count>0){
+            ans=st.Peek()+ans;
+            st.Pop();
+        }
+        return ans;
+    }
+}
