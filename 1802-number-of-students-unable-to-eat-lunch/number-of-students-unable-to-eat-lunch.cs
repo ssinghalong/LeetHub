a2bd96d08c0c8f6a59432c8fd3f3176c68b321e8
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-04-08 14:55:59 +0530

    Time: 55 ms (81.48%) | Memory: 40.2 MB (91.98%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cs
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cs
@@ -1,20 +1,34 @@
-public class Solution {
-    public int CountStudents(int[] students, int[] sandwiches) {
-        int uno=0,dos=0;
-        for(int i=0;i<students.Length;i++){
-            if(students[i]==1) dos++;
-            else uno++;
+public
+class Solution
+{
+public int CountStudents(int[] sandwiches, int[] students)
+    {
+        var st = new Stack<int>();
+        var qu = new Queue<int>();
+        for (int i = 0; i < students.Length; i++)
+        {
+            st.Push(students[students.Length-i-1]);
+            qu.Enqueue(sandwiches[i]);
         }
-        for(int i=0;i<sandwiches.Length;i++){
-            if(sandwiches[i]==0 && uno!=0){
-                uno--;
+        int t = -1;
+        while (t != st.Count && t != 0)
+        {
+            t = st.Count;
+            int m = t;
+            for (int i = 0; i < m; i++)
+            {
+                int front = qu.Dequeue();
+                if (st.Peek() == front)
+                {
+                    st.Pop();
+                }
+                else
+                {
+                    qu.Enqueue(front);
+                }
             }
-            else if(sandwiches[i]==1 && dos!=0){
-                dos--;
-            }
-            else break;
         }
         GC.Collect();
-        return uno+dos;
+        return qu.Count;
     }
 }
