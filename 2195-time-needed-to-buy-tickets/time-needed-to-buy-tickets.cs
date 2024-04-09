71f82af298801a56aef51de16adc8a56e16ae009
ssinghalong <120041042+ssinghalong@users.noreply.github.com>
2024-04-09 22:11:36 +0530

    Time: 72 ms (12.50%) | Memory: 40.5 MB (87.50%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cs
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cs
@@ -1,16 +1,15 @@
 public class Solution {
     public int TimeRequiredToBuy(int[] tickets, int k) {
+        int y = tickets[k];
         int t=0;
-        while(tickets[k]!=0){
-            for(int i=0;i<tickets.Length;i++){
-                if(tickets[i]!=0){
-                    tickets[i]=tickets[i]-1;
-                    t++;
-                }
-                if(tickets[k]==0){
-                    break;
-                }
+        for(int i=0;i<tickets.Length;i++){
+            if(tickets[i]<=y){
+                t+=tickets[i];
             }
+            else{
+                t+=y;
+            }
+            if(i>k && tickets[i]>=y) t--;
         }
         GC.Collect();
         return t;
